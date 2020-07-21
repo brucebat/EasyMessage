a26518fbcf19e39265b31d0e88ce59da58cf31d3
brucebat <brucewatchbatman@163.com>
2020-07-21 09:15:35 +0800

    初始化仓库

---
+++ src/main/java/com/brucebat/message/SpringBootStarterMessageApplication.java
@@ -0,0 +1,19 @@
+package com.brucebat.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author brucebat
+ * @date 2020/07/21
+ * @version 1.0
+ *
+ */
+@SpringBootApplication
+public class SpringBootStarterMessageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterMessageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brucebat/message/SpringBootStarterMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brucebat.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
