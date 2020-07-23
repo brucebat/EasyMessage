aa70ea44ff86e66711bb4de22f3ce22330b00a44
Sun Tianyu <suntianyu@shuwen.com>
2020-07-23 19:19:19 +0800

    feature(common): 新增markdown信息类中的属性和toMessage方法

--- src/main/java/com/brucebat/message/common/message/ding/MarkdownMessage.java
+++ src/main/java/com/brucebat/message/common/message/ding/MarkdownMessage.java
@@ -1,16 +1,41 @@
 package com.brucebat.message.common.message.ding;
 
+import com.brucebat.message.common.enums.MessagePropertiesEnum;
 import com.brucebat.message.common.enums.MessageTypeEnum;
+import com.google.gson.Gson;
 import lombok.Data;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @version 1.0
  * @author: Sun Tianyu
  * @date: Created in 2020/7/21
- * @description
+ * @description markdown格式消息类
  */
 @Data
 public class MarkdownMessage extends BaseMessage {
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 文本
+     */
+    private String text;
+    /**
+     * 消息接收人
+     */
+    private List<String> atMobiles;
+    /**
+     * 是否通知所有人
+     */
+    private boolean atAll;
 
     public MarkdownMessage(){
         this.msgType = MessageTypeEnum.MARKDOWN.getType();
@@ -18,6 +43,17 @@ public class MarkdownMessage extends BaseMessage {
 
     @Override
     public String toMessage() {
-        return null;
+        Gson gson = new Gson();
+        Map<String, Object> message = new HashMap<>(3);
+        message.put(MessagePropertiesEnum.MSG_TYPE.getValue(), this.msgType);
+        Map<String, Object> markdownItems = new HashMap<>(2);
+        markdownItems.put(MessagePropertiesEnum.TITLE.getValue(), this.title);
+        markdownItems.put(MessagePropertiesEnum.TEXT.getValue(), this.text);
+        message.put(MessagePropertiesEnum.MARKDOWM.getValue(), markdownItems);
+        Map<String, Object> contactItems = new HashMap<>(2);
+        contactItems.put(MessagePropertiesEnum.AT_MOBILES.getValue(), atMobiles);
+        contactItems.put(MessagePropertiesEnum.IS_AT_ALL.getValue(), atAll);
+        message.put(MessagePropertiesEnum.AT.getValue(), contactItems);
+        return gson.toJson(message);
     }
 }
